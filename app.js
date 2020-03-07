2c20c629025e74b254dbdceae0f12fc715ec5dab
Overview Team <andrei128@yahoo.com>
2020-03-07 20:45:31 +0200

    conectare la mongo in api

--- app.js
+++ app.js
@@ -5,21 +5,6 @@ const api = require('./routes/api')
 const app =express();
 
 
-
-const mongoose = require('mongoose');
-
-mongoose.connect(`mongodb://localhost:27017/proba`)
-.then(() => {
-    console.log('Connected to database');
-    // app.listen(port, () => {
-    //   console.log(`Express server listening on port ${port}`);
-    // });
-  });
-
-
-app.use('/',api);
-
-
 app.listen(PORT,function(){
     console.log(`Serverul merge pe portul ${PORT}`)
 })
--- routes/api.js
+++ routes/api.js
@@ -6,6 +6,24 @@ const http = require('http');
 const router = express.Router();
 
 
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb://localhost:27017/proba`)
+.then(() => {
+    console.log('Connected to database');
+    // app.listen(port, () => {
+    //   console.log(`Express server listening on port ${port}`);
+    // });
+  });
+
+
+app.use('/',api);
+
+
+
+
+
+
 router.get('/', (req, res) => {
     res.status(200);
     res.send('ok');

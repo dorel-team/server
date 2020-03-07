55723d55f038efc897003043645c1a863f166ded
Overview Team <andrei128@yahoo.com>
2020-03-07 20:47:29 +0200

    conectare la mongo in api

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const api = require('./routes/api')
 const app =express();
 
 
+
+app.use('/',api);
+
+
 app.listen(PORT,function(){
     console.log(`Serverul merge pe portul ${PORT}`)
 })
--- routes/api.js
+++ routes/api.js
@@ -17,10 +17,6 @@ mongoose.connect(`mongodb://localhost:27017/proba`)
   });
 
 
-app.use('/',api);
-
-
-
 
 
 

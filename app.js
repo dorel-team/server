002babe96235f4f8667af1504ccd8ee204f9a5e9
Overview Team <andrei128@yahoo.com>
2020-03-07 20:30:44 +0200

    conectare la mongo

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ const api = require('./routes/api')
 const app =express();
 
 
+
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb://localhost:27017/proba`)
+.then(() => {
+    console.log('Connected to database');
+    // app.listen(port, () => {
+    //   console.log(`Express server listening on port ${port}`);
+    // });
+  });
+
+
 app.use('/',api);
 
 
